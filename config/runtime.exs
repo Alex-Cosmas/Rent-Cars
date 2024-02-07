425d3efba9645a855328a5f0f64d40e39bec2021
AlexCsms <cosmasalex365@gmail.com>
2024-02-07 23:54:42 +0300

    Working release

--- config/runtime.exs
+++ config/runtime.exs
@@ -14,11 +14,14 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
+  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+
   config :rent_cars, RentCars.Repo,
     # ssl: true,
     # socket_options: [:inet6],
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2"),
+    socket_options: maybe_ipv6
 
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
@@ -27,14 +30,18 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
+
   config :rent_cars, RentCarsWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: port
     ],
     secret_key_base: secret_key_base
 
@@ -43,7 +50,12 @@ if config_env() == :prod do
   # If you are doing OTP releases, you need to instruct Phoenix
   # to start each relevant endpoint:
   #
-  #     config :rent_cars, RentCarsWeb.Endpoint, server: true
+
+  if System.get_env("PHX_SERVER") do
+    config :rent_cars, RentCarsWeb.Endpoint, server: true
+  end
+
+  # config :rent_cars, RentCarsWeb.Endpoint, server: true
   #
   # Then you can assemble a release by calling `mix release`.
   # See `mix help release` for more information.
