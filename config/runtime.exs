fff37ea6f5c5868670bcfe8cacc10f2158e9704f
Alex-Cosmas <cosmasalex365@gmail.com>
2024-02-15 09:09:54 +0300

    Releases fix

--- config/runtime.exs
+++ config/runtime.exs
@@ -6,6 +6,10 @@ import Config
 # and secrets from environment variables or elsewhere. Do not define
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
+if System.get_env("PHX_SERVER") do
+  config :rent_cars, RentCarsWeb.Endpoint, server: true
+end
+
 if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
@@ -14,12 +18,19 @@ if config_env() == :prod do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
+  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+
   config :rent_cars, RentCars.Repo,
     # ssl: true,
-    # socket_options: [:inet6],
     url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2"),
+    socket_options: maybe_ipv6
 
+  # The secret key base is used to sign/encrypt cookies and other secrets.
+  # A default value is used in config/dev.exs and config/test.exs but you
+  # want to use a different value for prod and you most likely don't want
+  # to check this value into version control, so we use an environment
+  # variable instead.
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
@@ -27,14 +38,18 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
+  host = System.get_env("PHX_HOST") || "example.com"
+  port = String.to_integer(System.get_env("PORT") || "4000")
+
   config :rent_cars, RentCarsWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: port
     ],
     secret_key_base: secret_key_base
 
@@ -42,9 +57,7 @@ if config_env() == :prod do
   #
   # If you are doing OTP releases, you need to instruct Phoenix
   # to start each relevant endpoint:
-  #
-      config :rent_cars, RentCarsWeb.Endpoint, server: true
-  #
+  # config :rent_cars, RentCarsWeb.Endpoint, server: true
   # Then you can assemble a release by calling `mix release`.
   # See `mix help release` for more information.
 
